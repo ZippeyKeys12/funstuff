3c026021ac92be6732290f6ae94e9679de541aef
Zain Aamer <ZippeyKeys12@gmail.com>
2019-12-29 18:07:35 -0400

    Make Ridged and Billow operators

---
+++ Assets/Scripts/Noise/Unary/Billow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Noise
+{
+    [Serializable]
+    public class Billow : Map
+    {
+        public Billow(Generator gen)
+            : base(gen)
+        { }
+
+        protected override Sample<T> TransformFunc<T>(Sample<T> a)
+        {
+            return 2 * Maths.Abs((dynamic)a) - 1;
+        }
+    }
+}
---
+++ Assets/Scripts/Noise/Unary/Ridged.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Noise
+{
+    [Serializable]
+    public class Ridged : Map
+    {
+        public Ridged(Generator gen)
+            : base(gen)
+        { }
+
+        protected override Sample<T> TransformFunc<T>(Sample<T> a)
+        {
+            return -2 * Maths.Abs((dynamic)a) + 1;
+        }
+    }
+}
