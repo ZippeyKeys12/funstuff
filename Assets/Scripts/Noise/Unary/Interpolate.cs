12ae3fb0373a9e30579b5106fbd967241f280dfe
Zain Aamer <ZippeyKeys12@gmail.com>
2020-01-03 15:17:05 -0600

    Remove invUnit

--- Assets/Scripts/Noise/Unary/Interpolate.cs
+++ Assets/Scripts/Noise/Unary/Interpolate.cs
@@ -8,7 +8,7 @@ namespace Noise
     [Serializable]
     public class Interpolate : Generator
     {
-        protected float unit, invUnit;
+        protected float unit;
         protected Generator gen;
         protected TweenType interp;
 
@@ -29,7 +29,6 @@ namespace Noise
             this.gen = gen;
             this.interp = interp;
             this.unit = unit;
-            invUnit = 1 / unit;
         }
 
         public override Sample<float> Get(float x, float frequency)
@@ -37,11 +36,11 @@ namespace Noise
             x *= frequency;
             frequency = 1;
 
-            var i = math.floor(x * invUnit) * unit;
+            var i = math.floor(x / unit) * unit;
 
-            var xr = new Sample<float>(x, 1) * invUnit;
+            var xr = new Sample<float>(x, 1) / unit;
 
-            var d = interp(xr - Maths.Floor(xr * invUnit) * unit);
+            var d = interp(xr - Maths.Floor(xr / unit) * unit);
 
             return Maths.Lerp(gen.Get(i, frequency), gen.Get(i + unit, frequency), d);
         }
@@ -51,16 +50,16 @@ namespace Noise
             xy *= frequency;
             frequency = 1;
 
-            var i = math.floor(xy * invUnit) * unit;
+            var i = math.floor(xy / unit) * unit;
 
             var xr = new Sample<float>(xy.x, 1);
             var yr = new Sample<float>(xy.y, 1);
 
-            var dx = interp(xr - Maths.Floor(xr * invUnit) * unit);
-            var dy = interp(yr - Maths.Floor(yr * invUnit) * unit);
+            var dx = interp(xr - Maths.Floor(xr / unit) * unit);
+            var dy = interp(yr - Maths.Floor(yr / unit) * unit);
 
-            var dxr = new Sample<float2>(dx.Value, new float2(dx.Gradient, 0)) * invUnit;
-            var dyr = new Sample<float2>(dy.Value, new float2(0, dy.Gradient)) * invUnit;
+            var dxr = new Sample<float2>(dx.Value, new float2(dx.Gradient, 0)) / unit;
+            var dyr = new Sample<float2>(dy.Value, new float2(0, dy.Gradient)) / unit;
 
             return Maths.Lerp(Maths.Lerp(gen.Get(i, frequency), gen.Get(i + new float2(unit, 0), frequency), dxr),
                               Maths.Lerp(gen.Get(i + new float2(0, unit), frequency), gen.Get(i + new float2(unit, unit), frequency), dxr),
@@ -72,19 +71,19 @@ namespace Noise
             xyz *= frequency;
             frequency = 1;
 
-            var i = math.floor(xyz * invUnit) * unit;
+            var i = math.floor(xyz / unit) * unit;
 
             var xr = new Sample<float>(xyz.x, 1);
             var yr = new Sample<float>(xyz.y, 1);
             var zr = new Sample<float>(xyz.z, 1);
 
-            var dx = interp(xr - Maths.Floor(xr * invUnit) * unit);
-            var dy = interp(yr - Maths.Floor(yr * invUnit) * unit);
-            var dz = interp(zr - Maths.Floor(zr * invUnit) * unit);
+            var dx = interp(xr - Maths.Floor(xr / unit) * unit);
+            var dy = interp(yr - Maths.Floor(yr / unit) * unit);
+            var dz = interp(zr - Maths.Floor(zr / unit) * unit);
 
-            var dxr = new Sample<float3>(dx.Value, new float3(dx.Gradient, 0, 0)) * invUnit;
-            var dyr = new Sample<float3>(dy.Value, new float3(0, dy.Gradient, 0)) * invUnit;
-            var dzr = new Sample<float3>(dz.Value, new float3(0, 0, dz.Gradient)) * invUnit;
+            var dxr = new Sample<float3>(dx.Value, new float3(dx.Gradient, 0, 0)) / unit;
+            var dyr = new Sample<float3>(dy.Value, new float3(0, dy.Gradient, 0)) / unit;
+            var dzr = new Sample<float3>(dz.Value, new float3(0, 0, dz.Gradient)) / unit;
 
             var j = Maths.Lerp(gen.Get(i, frequency), gen.Get(i + new float3(unit, 0, 0), frequency), dxr);
             var k = Maths.Lerp(gen.Get(i + new float3(0, 0, unit), frequency), gen.Get(i + new float3(unit, 0, unit), frequency), dxr);
