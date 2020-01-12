1c38a84b85746cbe48eeb75ed1eba64d6e407393
Zain Aamer <ZippeyKeys12@gmail.com>
2020-01-12 14:21:30 -0600

    Bias operator

---
+++ Assets/Scripts/Noise/Unary/Bias.cs
@@ -0,0 +1,21 @@
+using Unity.Mathematics;
+
+namespace Noise
+{
+    public class Bias : Map
+    {
+        protected float bias;
+
+        public Bias(Generator gen, float bias)
+            : base(gen)
+        {
+            this.bias = bias;
+        }
+
+        protected override Sample<T> TransformFunc<T>(Sample<T> a)
+        {
+            a = Maths.Clamp(2 * a - 1, -1, 1);
+            return 2 * (a + 1) / (bias * (a - 1) + 2) - 1;
+        }
+    }
+}
