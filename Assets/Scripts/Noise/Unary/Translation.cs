201fc5383bad663fc66a7ae68a1b8c79bf508b94
Zain Aamer <ZippeyKeys12@gmail.com>
2020-01-13 12:14:36 -0600

    No point complicating things

--- Assets/Scripts/Noise/Unary/Translation.cs
+++ Assets/Scripts/Noise/Unary/Translation.cs
@@ -2,13 +2,24 @@ using Unity.Mathematics;
 
 namespace Noise
 {
-    public class Translation : Transformation
+    public class Translation : Generator
     {
+        protected Generator gen;
+        protected float3 translation;
+
         public Translation(Generator gen, float3 translation)
-            : base(gen,
-                new float2x2(0, translation.x, 0, 1),
-                new float3x3(0, 0, translation.x, 0, 0, translation.y, 0, 0, 1),
-                float4x4.Translate(translation))
-        { }
+        {
+            this.gen = gen;
+            this.translation = translation;
+        }
+
+        public override Sample<float> Get(float x, float frequency)
+            => gen.Get(x - translation.x, frequency);
+
+        public override Sample<float2> Get(float2 xy, float frequency)
+            => gen.Get(xy - translation.xy, frequency);
+
+        public override Sample<float3> Get(float3 xyz, float frequency)
+            => gen.Get(xyz - translation, frequency);
     }
 }
